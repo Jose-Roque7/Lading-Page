9304250d99d369b2ede453f38c27beb7f28ba04f
Jose Roque <joseroque@msfissa.com>
2025-11-27 14:23:31 -0400

    Cambios

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 "use client"
-import App from "../src/App"
+
+import dynamic from "next/dynamic";
+
+const App = dynamic(() => import("../src/App"), {
+  ssr: false, // ⬅️ evita el error de hidratación
+});
+
 export default function Home() {
   return (
     <>
--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,27 @@ import { CTA } from './components/CTA';
 import { Footer } from './components/Footer';
 import { ThemeProvider } from './components/ui/themeMode';
 import { Toast } from './components/Toast';
+import { Loading } from './components/loader';
+import { useEffect, useState } from 'react';
+
 
 export default function App() {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const delay = 3000; // 3 segundos
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
     <div className="min-h-screen bg-white relative">
